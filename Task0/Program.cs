10f7b4cc165dfc2f70e095b17245ccf8731d3511
Ekyd <Ekyd777@gmail.com>
2022-04-14 08:32:43 +0300

    add task0

---
+++ Task0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задание №0: Вывести квадрат числа");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"Квадрат числа {number} ");
+Console.WriteLine(result);
