d55775003fdd3dcebba5918d34b93bedb7ea339f
Ekyd <Ekyd777@gmail.com>
2022-04-14 08:52:55 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задание 2: Даны 2 числа, показать большее и меньшее");
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max > number2 )
+{
+    Console.WriteLine($"Число {max} больше чем чиcло {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} больше чем чиcло {max}");
+}
